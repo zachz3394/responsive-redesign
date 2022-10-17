39283d59de50a76c044d80f1a5aae6ca50f448de
Zachary Zhu <zach.z3394@gmail.com>
2022-10-17 17:12:09 -0400

    add ads

--- scripts.js
+++ scripts.js
@@ -5,23 +5,27 @@ function scrollIn(id) {
 }
 
 function updateTableOfContentsHighlight() {
-    const scroll = $(document).scrollTop();
-    let highlight_id = null;
-    $('h1, h2, h3, h4, h5, h6').each((_, header) => {
-        const current_element = header;
-        const toc_id = '#toc_' + $(current_element).attr('id');
-        const elem_ypos = $(current_element).offset().top;
-        if (elem_ypos < scroll + 72) {
-            highlight_id = toc_id;
-        }
-        if ($(toc_id).length > 0) {
-            $(toc_id).removeClass('active');
-        }
-    });
-    // only highlight *last* element that is above the window's scroll position.
-    if ($(highlight_id).length > 0) {
-        $(highlight_id).addClass('active');
+  const scroll = $(document).scrollTop();
+  let highlight_id = null;
+  $('h1, h2, h3, h4, h5, h6').each((_, header) => {
+    const current_element = header;
+    const toc_id = '#toc_' + $(current_element).attr('id');
+    const elem_ypos = $(current_element).offset().top;
+    if (elem_ypos < scroll + 108) {
+      highlight_id = toc_id;
     }
+    if ($(toc_id).length > 0) {
+      $(toc_id).removeClass('active');
+    }
+  });
+  // only highlight *last* element that is above the window's scroll position.
+  if ($(highlight_id).length > 0) {
+    $(highlight_id).addClass('active');
+  }
 }
 
 window.onscroll = function() {updateTableOfContentsHighlight()};
+
+function hideAdBanner() {
+  document.getElementById('banner').hidden = true;
+}
